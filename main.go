0648fee3f101ce1f52fe09c7364c81ff73365bf4
Fransiscus Xaverius <xaverius.fransiscus078@gmail.com>
2024-06-29 23:27:08 +0700

    Added maps, if else statements, switch statements, and loops

--- main.go
+++ main.go
@@ -94,6 +94,104 @@ World`
 	} else {
 		fmt.Println(resultOfDivisionWithError)
 	}
+
+	//if else and switch cases
+	//if else
+	var myVar int = 10
+	if myVar > 5 {
+		fmt.Println("myVar is greater than 5")
+	} else if myVar < 5 {
+		fmt.Println("myVar is less than 5")
+	} else {
+		fmt.Println("myVar is equal to 5")
+	}
+
+	//switch
+	//conditional switch statement
+	switch myVar {
+		case 10:
+			fmt.Println("myVar is equal to 10")
+		case 5:
+			fmt.Println("myVar is equal to 5")
+		default:
+			fmt.Println("myVar is not equal to 10 or 5")
+	}
+
+	//non-conditional switch statement
+	switch {
+		case myVar > 5:
+			fmt.Println("myVar is greater than 5")
+		case myVar < 5:
+			fmt.Println("myVar is less than 5")
+		default:
+			fmt.Println("myVar is equal to 5")
+	}
+
+	//array
+	var myArray [3]int32 //this is an array with a length of 3 and the type of int32. because the value is not initialized, the array value of each element is 0 because the default value of an int32 is 0.
+	myArray[0] = 123
+	fmt.Println(myArray[0])
+	fmt.Println(myArray[1:3])
+	//array pointers
+	fmt.Println(&myArray[0]) //this will print the memory address of the first element of the array
+
+	//alternatives to array declaration
+	myArray2 := [3]int32{1, 2, 3} //myArray2 := [...]int32{1,2,3}
+	fmt.Println(myArray2)
+
+	var intSlice []int32 = []int32{4,5,6}
+	fmt.Println(intSlice)
+	//appending an array. this makes 7 to be appended to the end of the array as the last element.
+	intSlice = append(intSlice, 7) 
+	fmt.Println(intSlice)
+	//cap function to get the capacity of an array
+	fmt.Println(cap(intSlice))
+
+	//Spread operator to append multiple elements to an array
+	var intSlice2 []int32 = []int32{8,9,10}
+	intSlice = append(intSlice, intSlice2...)
+	fmt.Println(intSlice)
+
+	//map. A key-value pair data structure
+	var myTestMap map[string]uint8 = make(map[string]uint8) //this creates an empty map
+	fmt.Println(myTestMap)
+	myTestMap["key1"] = 1
+	fmt.Println(myTestMap)
+
+	//alternative way to make a map with predifined keys and values
+	var myMap map[string]int32 = map[string]int32{"key1": 1, "key2": 2}
+	fmt.Println(myMap)
+	fmt.Println(myMap["key1"])
+	//if you tried to access a key that doesn't exist you'll get the default value of the type of value of the map.
+	fmt.Println(myMap["key3"]) //this returns 0 because 0 is the default value of the type int32.
+
+	//check if key is in map
+	var num, ok = myMap["key3"] //to check if key value pair exists, make an 'ok' variable and use it to check if the key-value pair exists in the map
+	if ok{ //the ok variable will be true if the key is in the map
+		fmt.Println(num)
+	}else{
+		fmt.Println("key3" + " is not in the map")
+	}
+
+	//delete key-value pair from map
+	fmt.Println(myMap)
+	delete(myMap, "key1")
+	fmt.Println(myMap)
+
+	//for loops
+	for i := 0; i < 5; i++{
+		fmt.Printf("%v ", i)
+	}
+	fmt.Println()
+
+	//for range loops
+	var intSlice3 []int32 = []int32{11,12,13}
+	for i, value := range intSlice3{
+		fmt.Println(i, value)
+	}
+
+	//while loops
+
 }
 
 func myFunc(){ //this is a function in Go. It doesn't have a return value so it will not return anything.
